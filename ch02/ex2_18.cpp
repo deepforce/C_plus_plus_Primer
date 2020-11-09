eabc4087682751e294c026b4fb84d8c67872a78d
Zack <yuqz95@gmail.com>
2020-11-09 08:44:20 +0800

    Finish 2.3

---
+++ ch02/ex2_18.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() 
+{
+    int a = 0, b = 1;
+    int *p1 = &a, *p2 = p1;
+
+    // change the value of a pointer.
+    p1 = &b;
+    // change the value to which the pointer points
+    *p2 = b;
+}
